8a969677b16aabc036e1ab73ecd4fcece21c8140
skennel2 <skennel2@gmail.com>
2017-12-28 04:44:30 -0800

    인터페이스 분리

--- App/Domain/Unit/LivingCreature.cs
+++ App/Domain/Unit/LivingCreature.cs
@@ -166,13 +166,4 @@ namespace Srpg.App.Domain.Unit
             }
         }
     }    
-
-    public interface ICreatureBody
-    {
-        int NowHealthPoint {get;}
-        int MaxHealthPoint {get;}
-        void RecoveryHealth(int amount);
-        void TakeADamage(LivingCreature attacker, int amount);
-        void TakeADamageWithDepensiveRate(LivingCreature attacker, int amount);
-    }
 }
