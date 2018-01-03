2ecdea0c2a4efacbd6d2a4a35466b26c66e7a3cb
나윤수 <skennel2@gmail.com>
2018-01-03 11:01:11 +0900

    add Test

--- App/Test/TestResolutionRoot.cs
+++ App/Test/TestResolutionRoot.cs
@@ -2,16 +2,19 @@ using Srpg.App.Domain.Common;
 
 namespace Srpg.App.Test
 {
-    public class TestResolutionRoot : IResolutionRoot
+    public class TestResolutionRoot : ICompositionRoot
     {
+        private Test1 test1;
+
         public void SetUp()
         {
-            
+            test1 = new Test1();
         }
 
         public void Run()
         {
-            
+            test1.OneOnOneBattleTest();
+            test1.MapDrawTest();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,49 +12,9 @@ namespace Srpg
         {
             Console.WriteLine("Hello World!");  
 
-            OneOnOneBattleTest();
-            MapDrawTest();
+            
 
             Console.ReadKey();
-        } 
-
-        public static void OneOnOneBattleTest()
-        {
-            WarriorBaseFactoryTest factory = new WarriorBaseFactoryTest();
-            var w1 = factory.GetByName("test1");
-            var w2 = factory.GetByName("test2");
-
-            var oneOnOne = new OneOnOneConsoleBattle(w1, w2);
-            oneOnOne.StartBattle();
-        }      
-
-        public static void MapDrawTest()
-        {
-            Console.Clear();
-
-            WarriorBaseFactoryTest factory = new WarriorBaseFactoryTest();
-            var w1 = factory.GetByName("test1");
-
-            var map = new GridMap("test", 10, 10);
-            var defaultTile = new Tile("12", true, new TileConsoleShape('□'));
-            var tile2 = new Tile("23", true, new TileConsoleShape('◆'));
-
-            map.FillUpWith(defaultTile);
-            map.SetTile(tile2, 0, 0);
-            map.SetTile(tile2, 9, 9);
-            map.PutCreatureOn(w1, 1, 1, 1);
-
-            var c = map.GetLivingCreatureAt(1,1);
-            c.MoveDown();
-            c.MoveDown();
-            c.MoveDown();
-            c.MoveLeft();
-            c.MoveLeft();
-            Console.WriteLine(c.CretureXLocation.ToString() + "---" + c.CretureYLocation.ToString());
-
-            var mapDrawer = new ConsoleMapDrawer(map);
-            mapDrawer.ShowName();
-            mapDrawer.Draw();
         } 
     }
 }
